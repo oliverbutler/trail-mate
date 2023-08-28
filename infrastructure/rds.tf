e9ef84c62ff83e2cb479e5afcb3173dd69aede4e
Oliver Butler <dev@oliverbutler.uk>
2023-08-28 02:13:05 +0100

    fix: rds from ECS

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -21,12 +21,11 @@ resource "aws_security_group" "trail_mate_db_sg" {
   vpc_id = aws_vpc.trail_mate_vpc.id
   name   = "trail-mate-db-sg"
 
-  #  Allow traffic from within the VPC
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.trail_mate_vpc.cidr_block]
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.trail_mate_sg.id]
   }
 }
 

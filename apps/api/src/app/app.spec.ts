170fc16af2a99544bcfbd439913e667816aebdeb
Oliver Butler <dev@oliverbutler.uk>
2023-08-28 23:27:46 +0100

    test: use ts-rest to improve testing

--- apps/api/src/app/app.spec.ts
+++ apps/api/src/app/app.spec.ts
@@ -1,9 +1,31 @@
-import Fastify, { FastifyInstance } from "fastify";
+import Fastify, { FastifyInstance, InjectOptions } from "fastify";
 import { app } from "./app";
 import cuid from "cuid";
+import { initClient } from "@ts-rest/core";
+import { contract } from "@trail-mate/api-types";
+
 
 describe("App Test", () => {
   let server: FastifyInstance;
+
+  const testClient = initClient(contract, {
+    baseUrl: "",
+    baseHeaders: {},
+    api: async (args) => {
+
+      const response = await server.inject({
+        method: args.method as InjectOptions["method"],
+        url: args.path,
+        body: args.rawBody as InjectOptions["body"]
+      });
+
+      return {
+        status: response.statusCode,
+        body: response.json(),
+        headers: response.headers as unknown as Headers
+      };
+    }
+  });
 
   beforeEach(() => {
     server = Fastify();
@@ -11,34 +33,34 @@ describe("App Test", () => {
   });
 
   it("health endpoint should work", async () => {
-    const response = await server.inject({
-      method: "GET",
-      url: "/health"
-    });
 
-    expect(response.json()).toEqual({ status: "ok", uptime: expect.any(Number) });
+    const response = await testClient.getHealth();
+
+    expect(response).toEqual({
+      status: 200,
+      body: {
+        status: "ok",
+        uptime: expect.any(Number)
+      },
+      headers: expect.anything()
+    });
   });
 
   it("should return a list of tracks", async () => {
 
     const trackName = `Test ${cuid()}`;
 
-    const createResponse = await server.inject({
-      method: "POST",
-      url: "/tracks",
+    const createResponse = await testClient.createTrack({
       body: {
         name: trackName
       }
     });
 
-    expect(createResponse.statusCode).toBe(200);
+    expect(createResponse.status).toBe(200);
 
-    const response = await server.inject({
-      method: "GET",
-      url: "/tracks"
-    });
+    const response = await testClient.getTracks();
 
-    expect(response.json()).toStrictEqual(expect.arrayContaining([
+    expect(response.body).toStrictEqual(expect.arrayContaining([
       {
         id: expect.any(String),
         name: trackName
